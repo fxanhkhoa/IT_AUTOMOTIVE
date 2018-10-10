709240576167fc58194dbdf7dfc694836cbecb8a
fxanhkhoa <fxanhkhoa@gmail.com>
2018-10-10 15:21:31 +0700

    example Openni for python and Orbbec Astra

--- TX1_DRIVER/TestGPIO.py
+++ TX1_DRIVER/TestGPIO.py
@@ -15,7 +15,7 @@ on = 1
 
 #gpio187 -> button1
 #gpio186 -> button2
-#gpio219 -> button3
+#gpio163 -> button3
 #gpio184 -> button4
 
 #init led & button
@@ -23,10 +23,10 @@ led1 = jet.JETSON_GPIO(10)
 led2 = jet.JETSON_GPIO(9)
 led3 = jet.JETSON_GPIO(36)
 
-btn1 = jet.JETSON_GPIO(37)
-#btn2 = jet.JETSON_GPIO(186)
-#btn3 = jet.JETSON_GPIO(219)
-#btn4 = jet.JETSON_GPIO(184)
+btn1 = jet.JETSON_GPIO(187)
+btn2 = jet.JETSON_GPIO(186)
+btn3 = jet.JETSON_GPIO(163)
+btn4 = jet.JETSON_GPIO(511)
 
 #unexport
 try:
@@ -35,9 +35,9 @@ try:
 	led3.gpioUnexport()
 
 	btn1.gpioUnexport()
-	#btn2.gpioUnexport()
-	#btn3.gpioUnexport()
-	#btn4.gpioUnexport()
+	btn2.gpioUnexport()
+	btn3.gpioUnexport()
+	btn4.gpioUnexport()
 	print('unexported')
 except:
 	pass
@@ -50,18 +50,18 @@ led2.gpioExport()
 led3.gpioExport()
 
 btn1.gpioExport()
-#btn2.gpioExport()
-#btn3.gpioExport()
-#btn4.gpioExport()
+btn2.gpioExport()
+btn3.gpioExport()
+btn4.gpioExport()
 
 led1.gpioSetDirection(outputPin)
 led2.gpioSetDirection(outputPin)
 led3.gpioSetDirection(outputPin)
 
 btn1.gpioSetDirection(inputPin)
-#btn2.gpioSetDirection(inputPin)
-#btn3.gpioSetDirection(inputPin)
-#btn4.gpioSetDirection(inputPin)
+btn2.gpioSetDirection(inputPin)
+btn3.gpioSetDirection(inputPin)
+btn4.gpioSetDirection(inputPin)
 
 print('exported & set direction')
 led1.gpioSetValue(on)
@@ -76,29 +76,30 @@ while (True):
 	try:
 		#pass
 		value1 = (int)(btn1.gpioGetValue())
-		#value2 = (int)(btn2.gpioGetValue())
-		#value3 = (int)(btn3.gpioGetValue())
-		#value4 = (int)(btn4.gpioGetValue())
-		print(btn1.gpioGetValue())
+		value2 = (int)(btn2.gpioGetValue())
+		value3 = (int)(btn3.gpioGetValue())
+		value4 = (int)(btn4.gpioGetValue())
+		#print('3 -> ',btn3.gpioGetValue())
+		print('4 -> ',btn4.gpioGetValue())
 		if (value1 == 1):
 			led1.gpioSetValue(off)
 		else:
 			led1.gpioSetValue(on)
 		
-		#if (value2 == 1):
-		#	led2.gpioSetValue(on)
-		#else:
-		#	led2.gpioSetValue(off)
+		if (value2 == 1):
+			led2.gpioSetValue(off)
+		else:
+			led2.gpioSetValue(on)
 			
 		#if (value3 == 1):
-		#	led3.gpioSetValue(on)
-		#else:
 		#	led3.gpioSetValue(off)
+		#else:
+		#	led3.gpioSetValue(on)
 			
-		#if (value4 == 1):
-		#	led3.gpioSetValue(on)
-		#else:
-		#	led3.gpioSetValue(off)
+		if (value4 == 1):
+			led3.gpioSetValue(off)
+		else:
+			led3.gpioSetValue(on)
 			
 	except KeyboardInterrupt:
 		#led1.gpioUnexport()
